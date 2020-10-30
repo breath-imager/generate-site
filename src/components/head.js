aa302bc3bdb1a81890d43db41a1b57e490fc0db1
Ryan Pierre <info@ryanpierre.com>
2020-10-30 20:57:40 +0000

    rmeove head changes

--- src/components/head.js
+++ src/components/head.js
@@ -39,7 +39,7 @@ export default function Head() {
         rel="shortcut icon"
         type="image/x-icon"
       />
-      {/* 
+
       <script
         src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
@@ -56,7 +56,7 @@ export default function Head() {
         src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
         crossorigin="anonymous"
-      ></script> */}
+      ></script>
     </Helmet>
   )
 }
